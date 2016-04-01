17e720ac0ed3a892cfb39f62389899479ef08f9c
grahbari <guillaume.rahbari@amadeus.com>
2016-04-01 17:45:22 +0200

    big refactor tasks

--- gulp/tasks/gulp-copy.ts
+++ gulp/tasks/gulp-copy.ts
@@ -1,14 +1,47 @@
 import * as gulp from 'gulp';
+import {DEV_PATH, PROD_PATH, ALL_FILES, TYPESCRIPT_FILES, SASS_FILES} from '../gulp.conf';
 
 /**
- * This function copies files into the dist directory.
+ * @constant The array of excludes files.
  */
-function copyDist() {
+const EXCLUDED_FILES:string[] = [
+    '!' + TYPESCRIPT_FILES,
+    '!' + SASS_FILES
+];
 
-    return gulp.src(['**/*', '!**/*.ts', '!**/*.scss'], {cwd: 'src'})
-        .pipe(gulp.dest('dist/'));
+/**
+ * This function copies ALL_FILES excepts:
+ * <ul>
+ *   <li>Typescript files</li>
+ *   <li>Sass files</li>
+ * </ul>
+ * into the destinationDirectory directory.
+ *
+ * @param {string} destinationDirectory - The destination directory.
+ */
+function copyDist(destinationDirectory:string) {
+
+    const FILES:string[] = [ALL_FILES].concat(EXCLUDED_FILES);
+
+    return gulp.src(FILES, {base: 'src'})
+        .pipe(gulp.dest(destinationDirectory));
+}
+
+/**
+ * This function copies ALL_FILES into the DEV_PATH directory.
+ */
+function copyDev() {
+    return copyDist(DEV_PATH);
+}
+
+/**
+ * This function copies ALL_FILES into the PROD_PATH directory.
+ */
+function copyProd() {
+    return copyDist(PROD_PATH);
 }
 
 ///////////////////// Copy Tasks /////////////////////
 
-gulp.task('copy:dist', copyDist);
+gulp.task('copy:dev', copyDev);
+gulp.task('copy:prod', copyProd);
---
+++ manual_typings/connect-history-api-fallback.d.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by grahbari on 01/04/2016.
+ */
+
+declare module "connect-history-api-fallback" {
+    var historyApiFallback: any;
+    export = historyApiFallback;
+}
